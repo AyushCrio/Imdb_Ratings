ebbffcc0b3a194ab69ef295a1000b54d968933a5
Ayush Mishra <comingsooon24@gmail.com>
2023-06-17 23:40:42 +0530

    BookMyShow Movies Link

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -46,22 +46,16 @@ public class TestCases {
     
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
-        driver.navigate().to("https://www.linkedin.com/");
+        driver.get("https://in.bookmyshow.com/explore/home/chennai");
         driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//input[@autocomplete = \"username\"]")).sendKeys("Username");
-        driver.findElement(By.xpath("//input[@autocomplete = \"current-password\"]")).sendKeys("Password");
-        driver.findElement(By.xpath("//*[@id=\"main-content\"]/div/div/form/div[2]/button")).click(); //Click on sign-in
-        driver.findElement(By.xpath("//*[@id=\"ember1530\"]/div[2]")).click(); //Go to your profile
-        driver.findElement(By.id("ember1550")).click(); //Move to dashboard
-        String postImpression = driver.findElement(By.xpath("//*[@id=\"ember1863\"]/div/div")).getText();
-        String profileViews = driver.findElement(By.xpath("//*[@id=\"ember1865\"]/div/div")).getText();
-        driver.navigate().to("https://www.linkedin.com/feed/");
-        driver.findElement(By.id("ember1911")).click(); //Click to start post
-        driver.findElement(By.xpath("//*[@id=\"ember2155\"]/div/div[2]/div[1]/div/div/div/div/div/div/div[1]")).sendKeys("This is my first Post"); //Type something you want to share  
-        driver.findElement(By.xpath("//*[@id=\"share-to-linkedin-modal__header\"]/button")).click(); //Click on the drop down and select connection only 
-        driver.findElement(By.id("ember2180")).click(); //click to post
-        System.out.println("Post Impression" + postImpression);
-        System.out.println("Profile Views" + profileViews);
+        List<WebElement> allLinks = driver.findElements(By.xpath("//img[starts-with(@src, 'https://assets-in.bmscdn.com/discovery-catalog/events/')]"));
+        for(int i=0; i<allLinks.size(); i++)
+        {
+            System.out.println("Link:--> " + (allLinks.get(i)).getAttribute("src"));
+        }
+        String movieName = driver.findElement(By.xpath("//*[@id=\"https://in.bookmyshow.com/chennai/movies/por-thozhil/ET00357691-1\"]/div/div[3]/div[1]/div")).getText(); 
+        System.out.println("Movie name" + movieName);
+
         System.out.println("end Test case: testCase02");
     }
 
